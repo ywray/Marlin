10e622739b6f0857ee4a4d45ad7a230c9966a2c7
ywary <zyw067@126.com>
2022-06-12 16:50:32 +0800

    fix: 挤出头+硅胶套，重置PID

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -613,9 +613,9 @@
     #define DEFAULT_Ki_LIST {   1.08,   1.08 }
     #define DEFAULT_Kd_LIST { 114.00, 114.00 }
   #else
-    #define DEFAULT_Kp 27.23
-    #define DEFAULT_Ki 3.61
-    #define DEFAULT_Kd 51.31
+    #define DEFAULT_Kp 28.62
+    #define DEFAULT_Ki 3.64
+    #define DEFAULT_Kd 56.26
   #endif
 #endif // PIDTEMP
 
@@ -980,9 +980,9 @@
  *   M204 R    Retract Acceleration
  *   M204 T    Travel Acceleration
  */
-#define DEFAULT_ACCELERATION          1500    // X, Y, Z and E acceleration for printing moves
-#define DEFAULT_RETRACT_ACCELERATION  1500    // E acceleration for retracts
-#define DEFAULT_TRAVEL_ACCELERATION   1500    // X, Y, Z acceleration for travel (non printing) moves
+#define DEFAULT_ACCELERATION          1000    // X, Y, Z and E acceleration for printing moves
+#define DEFAULT_RETRACT_ACCELERATION  1000    // E acceleration for retracts
+#define DEFAULT_TRAVEL_ACCELERATION   1000    // X, Y, Z acceleration for travel (non printing) moves
 
 /**
  * Default Jerk limits (mm/s)
